d13849d4948645ef6a57a7c9d3b8ae7a3f0fbe87
cherry <hahungkk@gmail.com>
2020-09-22 01:17:28 +0700

    new code

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-use genesis::show::show_name;
 use std::collections::HashMap;
+use genesis::show::show_name;
 use std::fs::File;
 use std::io::Read;
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
-#[macro_use]
-extern crate rocket;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Gender {
     Male,
     Female,
     Other,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Person {
     first_name: String,
     last_name: String,
-    age: i8,
+    age: u8,
     gender: Gender,
 }
 
 impl Person {
+    fn new(first_name: String, last_name: String, age: u8) -> Person {
+        Person {
+            first_name,
+            last_name,
+            gender: Gender::Male,
+            age,
+        }
+    }
+
     fn show_info(&self) {
         println!(
             "{} {} is {:?}",
@@ -37,19 +39,25 @@ impl Person {
     fn get_full_name(&self) -> String {
         format!("Fullname is : {} {}", self.first_name, self.last_name)
     }
-}
 
-fn longer<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+    fn check_gender(&self) {
+        match self.gender {
+            Gender::Male => {
+                println!("is male");
+            }
+            Gender::Female => {
+                println!("is female")
+            }
+            _ => {
+                println!("GAYYYYYYY")
+            }
+        }
     }
 }
 
-#[get("/")]
-fn root() -> &'static str {
-    "Hello rocket web"
+fn apply_to_3<F>(f: F) -> i32
+    where F: Fn(i32) -> i32 {
+        f(3)
 }
 
 fn main() {
@@ -69,14 +77,10 @@ fn main() {
     new_hashmap.insert(&field_name, &field_value);
     println!("{}", new_hashmap.get(&field_name).unwrap());
 
-    match person.gender {
-        Gender::Male => {
-            println!("is male");
-        }
-        Gender::Female => println!("is female"),
-        _ => println!("GAYYYYYYY"),
-    }
+    person.check_gender();
+
     let f = File::open("Cherrylove.py");
+
     let mut res = match f {
         Result::Ok(file) => file,
         Result::Err(error) => {
@@ -85,39 +89,36 @@ fn main() {
             file
         }
     };
+
     let mut text = String::from("import os");
     res.read_to_string(&mut text).unwrap();
     println!("{}", text);
+
     let mut a: Vec<i32> = vec![1, 2, 3];
     a.push(1);
     println!("{:?}", a);
 
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("Spawn  : {}", i);
-            thread::sleep(Duration::from_micros(1));
-        }
-    });
+    let person2 = Person::new(String::from("Hung"), String::from("Ha"), 28);
+    println!("{:?}", person2);
+    person2.check_gender();
+    let mut person3 = person2.clone();
+    person3.gender = Gender::Female;
+    person3.check_gender();
+    person2.check_gender();
 
-    handle.join().unwrap();
+    let test = |i: i32| -> i32 { i + 1 };
+    let i = 1;
+    println!("{}", test(i));
+    let haystack = vec![1, 2, 3, 4];
+    let element = 1;
+    let element1 = 5;
 
-    for i in 10..15 {
-        println!("From main {}", i);
-        thread::sleep(Duration::from_micros(1));
-    }
+    let contain = |i| haystack.contains(i);
+    println!("{}", contain(&element,));
+    println!("{}", contain(&element1));
+    println!("There are {} element", haystack.len());
 
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let val = String::from("Hi");
-        tx.send(val).unwrap();
-    });
-    let rev = rx.recv().unwrap();
-    println!("I Got {}", rev);
+    let double = |x| x * 2;
 
-    let first = String::from("XXX");
-    let second = String::from("YYYYYY");
-    let longer = longer(first.as_str(), second.as_str());
-    println!("string which longer is {}", longer);
-
-    rocket::ignite().mount("/", routes![root]).launch();
+    println!("{}", apply_to_3(double));
 }
