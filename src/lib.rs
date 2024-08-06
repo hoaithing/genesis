8b53b3c7442a3b7870eef0ae1556591c62552834
cherry <hahungkk@gmail.com>
2024-08-06 22:15:18 +0700

    Add diesel

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,14 @@
-pub mod extras;
+pub mod models;
+pub mod schema;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
-use extras::extras::add;
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
 
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::fs::OpenOptions;
-use std::io::BufReader;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Point {
-    x: u32,
-    y: u32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CustomPerson {
-    pub name: String,
-    pub age: u8,
-    pub phones: Vec<String>,
-}
-
-pub fn number2text(number: u32) -> String {
-    let mapping: HashMap<u32, &str> = [
-        (0, ""),
-        (1, "Một"),
-        (2, "Hai"),
-        (3, "Ba"),
-        (4, "Bốn"),
-        (5, "Năm"),
-        (6, "Sáu"),
-        (7, "Bảy"),
-        (8, "Tám"),
-        (9, "Chín"),
-        (10, "Mười"),
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
-    return if number <= 10 {
-        mapping.get(&number).unwrap().to_string()
-    } else if number > 20 {
-        let unit_by_ten = mapping.get(&(&number / 10)).unwrap().to_string();
-        let unit_int = number % 10;
-        let mut unit = mapping.get(&unit_int).unwrap().to_string();
-        if unit == "0" {
-            return format!("{} Mươi", unit_by_ten);
-        } else if unit_int == 1 {
-            unit = String::from("Mốt");
-        }
-        format!("{} Mươi {}", unit_by_ten, unit)
-    } else {
-        let unit = mapping.get(&(&number - 10)).unwrap().to_string();
-        format!("Mười {}", unit)
-    };
-}
-
-pub fn call_add(a: u32, b: u32) -> u32 {
-    add(a, b)
-}
-
-pub fn get_text_data() -> CustomPerson {
-    let point = Point { x: 1, y: 2 };
-    let serialized = serde_json::to_string(&point).unwrap();
-    println!("serialized = {}", serialized);
-    let file = OpenOptions::new()
-        .read(true)
-        .open("Cherrylove.json")
-        .unwrap();
-    let reader = BufReader::new(file);
-    let deserialized: CustomPerson = serde_json::from_reader(reader).unwrap();
-    deserialized
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,22 @@
-use genesis::{call_add, get_text_data, number2text};
-use rand::Rng;
-use std::collections::HashMap;
+use diesel::prelude::*;
+use genesis::*;
+use genesis::models::Order;
+use genesis::schema::shop_module_order::dsl::*;
 
-#[derive(Debug, Copy, Clone)]
-enum Gender {
-    Male,
-    Female,
-    Other,
-}
-
-#[derive(Debug, Clone)]
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8,
-    gender: Gender,
-}
-
-impl Person {
-    fn new(first_name: String, last_name: String, age: u8) -> Person {
-        Person {
-            first_name,
-            last_name,
-            gender: Gender::Male,
-            age,
-        }
-    }
-
-    fn show_info(&self) {
-        println!("{} is {:?}", self.get_full_name(), self.gender);
-    }
-
-    fn get_full_name(&self) -> String {
-        format!("Fullname is : {} {}", self.first_name, self.last_name)
-    }
-
-    fn check_gender(&self) {
-        match self.gender {
-            Gender::Male => {
-                println!("is male");
-            }
-            Gender::Female => println!("is female"),
-            _ => println!("GAYYYYYYY"),
-        }
-    }
-}
-
-fn apply_to_3<F>(f: F) -> i32
-where
-    F: Fn(i32) -> i32,
-{
-    f(3)
-}
-
-#[allow(dead_code)]
-fn get_url(url: &str) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get(url)?.json::<HashMap<String, String>>()?;
-    Ok(resp)
-}
-
-fn is_odd(x: u32) -> bool {
-    x % 2 == 1
-}
 
 fn main() {
-    let mut person = Person {
-        first_name: String::from("Cherry"),
-        last_name: String::from("Love"),
-        age: 28,
-        gender: Gender::Male,
-    };
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-    let mut new_hashmap = HashMap::new();
+    let connection = &mut establish_connection();
+    let results = shop_module_order.filter(esim.eq(true))
+        .limit(10)
+        .select(Order::as_select() )
+        .order_by(created.desc())
+        .load(connection)
+        .expect("Error loading orders");
 
-    new_hashmap.insert(&field_name, &field_value);
-    println!("{}", new_hashmap.get(&field_name).unwrap());
-
-    person.gender = Gender::Other;
-    person.show_info();
-    person.check_gender();
-
-    let mut a: Vec<i32> = vec![1, 2, 3];
-    a.push(1);
-    println!("{:?}", a);
-
-    let person2 = Person::new(String::from("Hung"), String::from("Ha"), 28);
-    println!("{:?}", person2);
-    person2.check_gender();
-    let mut person3 = person2.clone();
-    person3.gender = Gender::Female;
-    person3.check_gender();
-    person2.check_gender();
-
-    let test = |i: i32| -> i32 { i + 1 };
-    let i = 1;
-    println!("{}", test(i));
-    let haystack = vec![1, 2, 3, 4];
-    let element = 1;
-    let element1 = 5;
-
-    let contain = |i| haystack.contains(i);
-    println!("{}", contain(&element,));
-    println!("{}", contain(&element1));
-    println!("There are {} element", haystack.len());
-
-    let double = |x: i32| -> i32 { x * 2 };
-
-    println!("{}", apply_to_3(double));
-    //    let url = "https://httpbin.org/ip";
-    //    let res = get_url(url).unwrap();
-    //    println!("{:#?}", res);
-    let new_fun = (0..20).filter(|&x| is_odd(x)).fold(0, |acc, x| acc + x);
-    println!("{}", new_fun);
-    println!("{}", call_add(10, 20));
-
-    let person = get_text_data();
-    println!("{}", person.name);
-    println!("{}", person.age);
-    println!("{:?}", person.phones);
-    let mut rand = rand::thread_rng();
-    let number = rand.gen_range(0, 99);
-    println!("{}: {}", number, number2text(number))
+    println!("Displaying {} orders", results.len());
+    for order in results {
+        println!("{}", order.receiver_name);
+        println!("-----------\n");
+        println!("{}", order.created);
+    }
 }
