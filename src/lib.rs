f1a7b07f103d90bdf8575237f5dc42f05b7f7287
cherry <hahungkk@gmail.com>
2024-08-07 17:02:11 +0700

    add email

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 pub mod models;
 pub mod schema;
+
+use std::collections::HashMap;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
+use reqwest::header::{ACCEPT, HeaderMap, CONTENT_TYPE, AUTHORIZATION};
 use rocket::get;
 use rocket::serde::json::Json;
 
@@ -27,4 +30,35 @@ pub fn list_orders() -> Json<Vec<Order>>{
         .load(connection)
         .expect("Error loading orders");
     Json(results)
+}
+
+
+pub fn send_zepto_email(send_to: &str, uid: &str) {
+    let mut post_body = HashMap::new();
+    post_body.insert("template_key", env::var("QR_ORDER_KEY")
+        .expect("Invalid QR_ORDER_KEY")
+        .as_str());
+    let mut from = HashMap::new();
+
+    from.insert("address", "noreply@xplori.world");
+    from.insert("name", "Xplori");
+
+    post_body.insert("from", from);
+
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+    headers.insert(AUTHORIZATION, env::var("ZEPTO_MAIL_API_KEY")
+        .expect("Invalid ZEPTO_MAIL_API_KEY")
+        .parse()
+        .unwrap());
+    // let client = reqwest::blocking::Client::builder().default_headers(headers).build().unwrap();
+    let client = reqwest::blocking::Client::new();
+    let res = client.post("https://api.zeptomail.com/v1.1/email/template")
+        .headers(headers)
+        .send()
+        .unwrap()
+        .json()
+        .unwrap();
+    println!("{}", res);
 }
